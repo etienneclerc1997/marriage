953116020971d47683f533630ae9b43a5f130548
ClercE <etienne.clerc@cegepvicto.info>
2018-03-28 10:31:32 -0400

    first commit

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -5,14 +5,23 @@ import { AppRoutingModule } from './app-routing.module';
 
 import { AppComponent } from './app.component';
 
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+import { MaterialModule } from './material.module';
+import { DialogContributeComponent } from './dialog-contribute/dialog-contribute.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    DialogContributeComponent
+  ],
+  entryComponents: [
+    DialogContributeComponent,
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    BrowserAnimationsModule,
+    MaterialModule
   ],
   providers: [],
   bootstrap: [AppComponent]
